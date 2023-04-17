d732ce2eb7c1f3bd50a2c7395de37d289e7d9528
Lana <svetlnovikova@gmail.com>
2023-04-17 19:33:02 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 
 echo "Using Docker version: $docker_version"
 
+docker build --build-arg DOCKER_VERSION=$docker_version -t writerside-builder:$docker_version .
 /opt/builder/bin/idea.sh helpbuilderinspect -source-dir . -product $PRODUCT --runner github -output-dir artifacts/ || true
 echo "Test existence of $ARTIFACT artifact"
 test -e artifacts/$ARTIFACT
